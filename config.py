7dc1dbf07b33b71e63bc7594ac0b76157b7c97a6
Ben Kedl <benj.kedl@gmail.com>
2018-08-14 21:48:02 -0700

    added function to average time of goals

---
+++ config.py
@@ -0,0 +1,7 @@
+consumer_key = 'G6mKRZznL8JHNYp4e1CyzQM6M'
+consumer_secret =  'Mj4DrtCdt9FmHUVJjdHMvXqPSoMwOjcgBxXUAlf8ehCMYtl1gY'
+
+access_token= '1023983379400679431-klY3tRPVsINsuESebtJTgAup2Lnnme'
+access_token_secret= 'ZrTlzSOfDp2VmEg83Fe1Ktsxg7Eny9h8qm03UDs81GKLx'
+
+g_key = 'AIzaSyDSjX-Di5XQ4FRBx0ZYGw59OlHTXDQj9j8'
